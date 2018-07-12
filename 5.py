b5d996d53e8c83a6ed644b3ff30c11cec3850874
Greeshmapatel <40879739+Greeshmapatel@users.noreply.github.com>
2018-07-12 14:43:54 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,n3;
+    scanf("%d %d %d", &n1,&n2,&n3);
+    
+    if(n1>=n2 && n2>=n3)
+      print("%d",n1);
+      
+      if(n2>=n3 && n2>=n1)
+       print("%d",n2);
+       
+       if(n3>=n1 && n3>=n2)
+        print("%d", n3);
+         return 0;
+         }
+    
