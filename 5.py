92b008cb5b0c6f2c744e5911d36030b2ac0e438a
Greeshmapatel <40879739+Greeshmapatel@users.noreply.github.com>
2018-07-12 14:46:58 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -5,13 +5,13 @@ int main()
     scanf("%d %d %d", &n1,&n2,&n3);
     
     if(n1>=n2 && n2>=n3)
-      print("%d",n1);
+      printf("%d",n1);
       
       if(n2>=n3 && n2>=n1)
-       print("%d",n2);
+       printf("%d",n2);
        
        if(n3>=n1 && n3>=n2)
-        print("%d", n3);
+        printf("%d", n3);
          return 0;
          }
     
